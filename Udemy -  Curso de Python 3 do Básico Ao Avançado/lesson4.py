2e3ce21a115454d6e3f7ecc63394d367e5aec406
necomurilo <109995111+necomurilo@users.noreply.github.com>
2023-09-10 01:18:31 -0300

    lesson-4e5

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson4.py
@@ -0,0 +1,33 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+#positivo ou negativo. int sem sinal é considerado positivo
+# print(11) # int
+# print(-11) # int
+# print(0) 
+
+# Control / comenta o selecionado
+
+
+# Float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# Positivo ou negativo com ponto flutuante float sem sinal é considerado positivo
+# print(1.5, -1.25) # decimal
+
+# A funçào type mostra o tipo que o Python
+# inferiu ao valor
+print(type('Neco')) 
+print(type(0)) 
+print(type(1.1), type(-1.1), type(0.0)) 
+
+
+# Tudo em Python é um objeto
+# Tudo em Python é um objeto
+# Tudo em Python é um objeto
+# Tudo em Python é um objeto
+# Tudo em Python é um objeto
+
+
+print(          )
+print(          )
+print(          )
---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson5.py
@@ -0,0 +1,8 @@
+# tipo de dado bool (boolean)
+# ao questionar algo em um programa, só existem duas respostas possíveis:
+# True ou False
+# existem vários operadores para "questionar"
+# dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
+print(10 == 10) # True
+print(10 > 11) # False
+print(type(10 == 10)) #classe bool
