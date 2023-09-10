8521d1b22afeb49051c553ab78fed69b2d868a32
necomurilo <109995111+necomurilo@users.noreply.github.com>
2023-09-09 23:37:47 -0300

    organized-leasson2

--- Udemy -  Curso de Python 3 do Básico Ao Avançado/Lesson3.py
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/Lesson3.py
@@ -1,9 +1,3 @@
-print(4 , 12, 1994, sep='-', end= '\n') #sep é separador, altera o espaço de separação do print
-print(1, 2, sep="/", end= "\n")
-print(1, 100, sep="-", end= "\n")
-# CRLF = quebra de linha /r/n padrão do windowns
-# LF = /n linux unix 
-
 """
 DocString
 Python = Linguagem de programação
