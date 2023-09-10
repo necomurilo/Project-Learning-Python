cbeac2cdb8b0d311dd8b71479ef8547b302ff7c4
necomurilo <109995111+necomurilo@users.noreply.github.com>
2023-09-09 23:33:21 -0300

    Lesson 09.09.23

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson1.py
@@ -0,0 +1,11 @@
+# Permite escrever um comentário
+print(123) # Na frente (comentar)
+# Evitar colocar em baixo (na frente da # nada será exibido ou executado)
+print(456)
+"""
+Não é comentário, o python sempre lê, mas permite criar linhas.  (DocString) 
+"""
+
+'''
+serve para escrever ou explciar código.
+'''
---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson2-3.py
@@ -0,0 +1,33 @@
+print(4 , 12, 1994, sep='-', end= '\n') #sep é separador, altera o espaço de separação do print
+print(1, 2, sep="/", end= "\n")
+print(1, 100, sep="-", end= "\n")
+# CRLF = quebra de linha /r/n padrão do windowns
+# LF = /n linux unix 
+
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem - Dinâmica / Forte
+str -> Strings -> Texto
+Strings são textos que estáo dentro de aspas
+"""
+print(123456789)
+
+
+# Aspas simples
+print('Neco')
+
+
+# Aspas duplas
+print("Murilo Neco")
+print('Murilo "Neco"')
+#pode usar aspas duplas dentro de aspas simples ou o contrário também
+
+
+# Escape
+print("Murilo \"Neco")
+#usado para ignorar a utilização de duas aspas
+
+
+# r
+print(r"Murilo \"Neco\"")
