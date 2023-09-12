0d66c592c062c502988ee646404baa28ea42a407
necomurilo <109995111+necomurilo@users.noreply.github.com>
2023-09-11 23:03:52 -0300

    upgrade até aula 8

--- Udemy -  Curso de Python 3 do Básico Ao Avançado/leasson6.py
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/leasson6.py
@@ -5,3 +5,4 @@
 print(int('1'), type(int('1')))
 print(float('1') + 1)
 print(bool(' '))
+print(str(11) + 'b')
---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/leasson8.py
@@ -0,0 +1,14 @@
+nome = 'Murilo'
+sobrenome = 'Neco'
+idade = int(28)
+ano_nascimento = 2023 - idade # desse as vezes da aprox. o que utilizei foi ano_nascimento = int(1994)
+maior_de_idade = idade >= 18
+altura_metros = float(1.96)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
