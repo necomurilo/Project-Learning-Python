9ae3cd9e11ebbc22a74588fd8ae26081d83836bc
necomurilo <109995111+necomurilo@users.noreply.github.com>
2023-09-11 22:22:38 -0300

    leasson6

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/leasson6.py
@@ -0,0 +1,7 @@
+# conversão de tipos, coerção
+# type convertion, tupecasting, coercion
+# tipos imutáveis e primitivos: 
+# Str, int, float, bool
+print(int('1'), type(int('1')))
+print(float('1') + 1)
+print(bool(' '))
