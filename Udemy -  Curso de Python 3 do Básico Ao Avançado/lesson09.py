ef055ba08defe8f8b7d2bbde2dc1c97644d0658c
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-14 20:07:12 -0300

    lesson09 - operacoes

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson09.py
@@ -0,0 +1,25 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 12
+print('Subitração', subtracao)
+
+multiplicacao = 8 * 8
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3 # sempre retorna float 
+print('Divisão', divisao)
+
+divisao_inteira = 100 // 3 # número truncado, ela corta e retorna um número inteiro (CORTA)
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10 
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # Resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0) # para saber o resto e facilitar na divisão
+print(16 % 8 == 0)
+print(10 % 2 == 0) # saber se o número é par
+print(15 % 2 == 0)
