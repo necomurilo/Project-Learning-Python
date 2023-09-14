99d88228aa4af0d57ff610ebeeeb1924b3c98628
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-13 22:36:18 -0300

    settingsjason - configuraçoes

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/Json/settingsjason.py
@@ -0,0 +1,17 @@
+#Settings Json
+{
+    "workbench.iconTheme": "material-icon-theme",
+    "files.autoSave": "afterDelay",
+    "git.ignoreMissingGitWarning": true,
+    "editor.wordWrap": "on",
+    "editor.fontSize": 20,
+    "explorer.compactFolders": false,
+    "code-runner.runInTerminal": true,
+    "code-runner.clearPreviousOutput": true,
+    "code-runner.executorMap": {
+        "python": "cls ; python -u", 
+    },
+    "code-runner.ignoreSelection": true,
+    "workbench.colorTheme": "OM Theme (Default Dracula Italic)",
+    "python.defaultInterpreterPath": "python"
+    }
