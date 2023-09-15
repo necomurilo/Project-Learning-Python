eadca7424d208a38d0eefc1aae806bbd366666bc
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-15 19:16:45 -0300

    Calculadora free
    Calculadora de IMC para todos

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/Calculadora_IMC.py
@@ -0,0 +1,7 @@
+# Substitua e calcule
+# Fill in and cacule 
+nome =' xxxxx '
+altura = 1111
+peso = 1111
+imc = peso / (altura)**2
+print(nome,"tem", altura,'de altura','pesa', peso, 'quilos e seu IMC é', imc)
