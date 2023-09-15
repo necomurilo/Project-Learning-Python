48b89b9277788e084eb8a7e12f31dac18f1fcbfa
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-15 19:13:09 -0300

    correções e calculadora IMC

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson10.py
@@ -0,0 +1,7 @@
+concatenatecao = 'Murilo' + ' Lima' + ' Neco'
+print(concatenatecao)
+
+a_dez_vezes = 'A' * 10 # para fazer a repetição de uma letra
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson12_C_IMC.py
@@ -0,0 +1,16 @@
+# Calculadora de IMC
+
+#EXEMPLO
+nome = 'Murilo Lima Neco'
+altura = 1.96
+peso = 102
+imc = peso / (altura)**2
+print(nome,"tem", altura,'de altura','pesa', peso, 'quilos e seu IMC é', imc)
+
+
+#PREENCHA e calcule
+nome =' xxxxx '
+altura = 1111
+peso = 1111
+imc = peso / (altura)**2
+print(nome,"tem", altura,'de altura','pesa', peso, 'quilos e seu IMC é', imc)
