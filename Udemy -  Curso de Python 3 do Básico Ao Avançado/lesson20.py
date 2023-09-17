e5fe4d1752641a61dfaca4c6b90ea08cbec95243
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-17 09:43:23 -0300

    Create lesson20

---
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/lesson20.py
@@ -0,0 +1,19 @@
+#primeiro_valor = input('Digite um valor: ')
+#segundo_valor = input('Digite um valor: ')
+#if primeiro_valor >= segundo_valor:
+#    print('O primeiro valor é maior que o segundo.')
+#else:
+#    print('O segundo valor é amior que o primerio. ')
+
+
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite um valor: ')
+if primeiro_valor >= segundo_valor:
+    print(
+        f'O {primeiro_valor=} é maior ou igual ' f'ao que {segundo_valor=}.')
+else:
+    print(
+        f'O {segundo_valor=} é maior ou igual' 
+        f'do que {primeiro_valor=}.'
+        )
