5012e7f7a4ca46a2d375479b18dbd4006530815d
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-16 22:33:53 -0300

    Calculadora IMC-2
    simplificando o cod

--- Udemy -  Curso de Python 3 do Básico Ao Avançado/Calculadora_IMC.py
+++ Udemy -  Curso de Python 3 do Básico Ao Avançado/Calculadora_IMC.py
@@ -1,7 +1,24 @@
 # Substitua e calcule
-# Fill in and cacule 
-nome =' xxxxx '
+# Fill in and cacule
+
+
+
+# Calculadora de IMC - 1
+nome ='xxxxx'
 altura = 1111
 peso = 1111
 imc = peso / (altura)**2
 print(nome,"tem", altura,'de altura','pesa', peso, 'quilos e seu IMC é', imc)
+
+
+# Calculadora de IMC - 2
+nome = 'XXXXXXXXXXX'
+altura = 1.80
+peso = 111
+imc = peso / (altura)**2
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+print(linha_1)
+print(linha_2)
+print(linha_3)
