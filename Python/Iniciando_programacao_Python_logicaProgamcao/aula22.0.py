79af4b2c7f079adf206ebdcd4ffc927861dd377e
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-21 20:52:59 -0300

    or

---
+++ Python/Iniciando_programacao_Python_logicaProgamcao/aula22.0.py
@@ -0,0 +1,12 @@
+# or - Qualquer condição verdadeira avalia toda a expressão como verdadeira.
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+
+senha_permitida = '123456'
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
---
+++ Python/Iniciando_programacao_Python_logicaProgamcao/aula22.1.py
@@ -0,0 +1,3 @@
+#print(0 or False or 0 or 'abc' or True)
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
