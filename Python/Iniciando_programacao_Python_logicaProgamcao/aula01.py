41c3dd64c9beba75c08f73a838da9b3e97c46585
Murilo Lima Neco <muriloneco@gmail.com>
2023-10-15 20:00:50 -0300

    revisão

--- Python/Iniciando_programacao_Python_logicaProgamcao/aula01.py
+++ Python/Iniciando_programacao_Python_logicaProgamcao/aula01.py
@@ -1,6 +1,8 @@
+################################
 # Permite escrever um comentário
 print(123) # Na frente (comentar)
 # Evitar colocar em baixo (na frente da # nada será exibido ou executado)
+
 print(456)
 """
 Não é comentário, o python sempre lê, mas permite criar linhas.  (DocString) 
--- Python/Iniciando_programacao_Python_logicaProgamcao/aula02.py
+++ Python/Iniciando_programacao_Python_logicaProgamcao/aula02.py
@@ -1,5 +1,10 @@
+# SEP - é um separador = 10-10-10
 print(4 , 12, 1994, sep='-', end= '\n') #sep é separador, altera o espaço de separação do print
 print(1, 2, sep="/", end= "\n")
 print(1, 100, sep="-", end= "\n")
-# CRLF = quebra de linha /r/n padrão do windowns
-# LF = /n linux unix 
+
+
+# CRLF = quebra de linha \r\n padrão do windowns
+
+
+# LF = \n linux unix 
