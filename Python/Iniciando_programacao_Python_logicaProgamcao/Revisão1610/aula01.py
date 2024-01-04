ab4124200a29c645c3f188105ac88bec74481ea9
Murilo Lima Neco <muriloneco@gmail.com>
2024-01-04 19:17:11 -0300

    Update aula01.py

--- Python/Iniciando_programacao_Python_logicaProgamcao/Revisão1610/aula01.py
+++ Python/Iniciando_programacao_Python_logicaProgamcao/Revisão1610/aula01.py
@@ -3,7 +3,7 @@
 print(123) # Na frente (comentar)
 # Evitar colocar em baixo (na frente da # nada será exibido ou executado)
 
-print(456)
+print(Asuka - Unit00)
 """
 Não é comentário, o python sempre lê, mas permite criar linhas.  (DocString) 
 """
