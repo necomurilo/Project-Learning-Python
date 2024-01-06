87e9defc5c78317f4ef08ed4f39e7f93b81d9c15
Murilo Lima Neco <muriloneco@gmail.com>
2024-01-05 23:02:13 -0300

    Update aula01.py

--- Python/Iniciando_programacao_Python_logicaProgamcao/Revisão1610/aula01.py
+++ Python/Iniciando_programacao_Python_logicaProgamcao/Revisão1610/aula01.py
@@ -3,7 +3,7 @@
 print(123) # Na frente (comentar)
 # Evitar colocar em baixo (na frente da # nada será exibido ou executado)
 
-print(Asuka | Unit-00)
+print(Asuka | Unit-00 /StayByMy - FFXV)
 """
 Não é comentário, o python sempre lê, mas permite criar linhas.  (DocString) 
 """
